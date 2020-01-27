d1aea7fc3a81e8a088f1f38cad709c6df30a718a
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-01-27 12:00:35 +0100

    [BUGFIX] Remove wrong replace configuration

---
+++ Tests/Unit/Mailer/SingleRecipientPluginFactoryTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ssch\SwiftmailerSingleRecipient\Tests\Unit\Mailer;
+
+use Ssch\SwiftmailerSingleRecipient\Mailer\SingleRecipientPluginFactory;
+
+class SingleRecipientPluginFactoryTest extends \PHPUnit_Framework_TestCase
+{
+
+}
